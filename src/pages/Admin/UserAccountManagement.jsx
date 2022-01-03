158b46279b8f0946a77fac5640a344f580b29dbd
dinhphilong16 <78018754+dinhphilong16@users.noreply.github.com>
2022-01-04 01:05:33 +0700

    fix table

--- src/pages/Admin/UserAccountManagement.jsx
+++ src/pages/Admin/UserAccountManagement.jsx
@@ -3,89 +3,123 @@ import Footer from '../../components/Admin-Footer';
 import Sidebar from '../../components/Admin-Sidebar';
 
 function UserAccountManagement() {
-    return (
+  return (
+    <div>
+      <Header />
+      <div className='useraccount-content'>
+        <Sidebar />
+        <div className='useraccount-table'>
+          <div className='table-container'>
+            <table id='table' className='table-uam'>
+              <thead>
+                <tr>
+                  <th> ID </th>
+                  <th> Username </th>
+                  <th> Loại Tài Khoản </th>                 
+                  <th> Loại Tài Khoản </th>      
+                  <th> Loại Tài Khoản </th>                  
+                  <th> Loại Tài Khoản </th>
+                  
+                  
 
-        <div>
-            <Header />
-            <div className='useraccount-content'>
-
-                <Sidebar />
-                <div className='useraccount-table'>
-                    <table id="table" class="table">
-                        <thead>
-                            <tr>
-                                <th> ID </th>
-                                <th> Username </th>
-                                <th> Loại Tài Khoản </th>
-                            </tr>
-                        </thead>
-                        <tbody id="tbody">
-                            <tr>
-                                <td>Item 1</td>
-                                <td>Item 2</td>
-                                <td>Item 3</td>
-                            </tr>
-                            <tr>
-                                <td>Item 1</td>
-                                <td>Item 2</td>
-                                <td>Item 3</td>
-                            </tr>
-                            <tr>
-                                <td>Item 1</td>
-                                <td>Item 2</td>
-                                <td>Item 3</td>
-                            </tr>
-                            <tr>
-                                <td>Item 1</td>
-                                <td>Item 2</td>
-                                <td>Item 3</td>
-                            </tr>
-                            <tr>
-                                <td>Item 1</td>
-                                <td>Item 2</td>
-                                <td>Item 3</td>
-                            </tr>
-                            <tr>
-                                <td>Item 1</td>
-                                <td>Item 2</td>
-                                <td>Item 3</td>
-                            </tr>
-                            <tr>
-                                <td>Item 1</td>
-                                <td>Item 2</td>
-                                <td>Item 3</td>
-                            </tr>
-                            <tr>
-                                <td>Item 1</td>
-                                <td>Item 2</td>
-                                <td>Item 3</td>
-                            </tr>
-                            <tr>
-                                <td>Item 1</td>
-                                <td>Item 2</td>
-                                <td>Item 3</td>
-                            </tr>
-                            <tr>
-                                <td>Item 1</td>
-                                <td>Item 2</td>
-                                <td>Item 3</td>
-                            </tr>
-                            <tr>
-                                <td>Item 1</td>
-                                <td>Item 2</td>
-                                <td>Item 3</td>
-                            </tr>
-                            <tr>
-                                <td>Item 1</td>
-                                <td>Item 2</td>
-                                <td>Item 3</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <Footer />
+                </tr>
+              </thead>
+              <tbody id='tbody'>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+                <tr>
+                  <td>Item 1</td>
+                  <td>Item 2</td>
+                  <td>Item 3</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-    );
+      </div>
+      <Footer />
+    </div>
+  );
 }
 export default UserAccountManagement;
