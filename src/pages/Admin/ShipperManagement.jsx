b4a4790f9eab9a408184c49ffc618a6b6d63c17a
QuocTuan18CLC1 <QuocTuan18CLC1>
2022-01-08 21:50:42 +0700

    fetch data for client, customer, store, shipper, order

--- src/pages/Admin/ShipperManagement.jsx
+++ src/pages/Admin/ShipperManagement.jsx
@@ -2,7 +2,32 @@
 import Footer from '../../components/Admin-Footer';
 import Sidebar from '../../components/Admin-Sidebar';
 
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 function ShipperManagement() {
+  const [shippers, setShippers] = useState()
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/shipper/list")
+      .then(res => {
+        setShippers(res.data)
+      })
+  }, [])
+
+  shippers && shippers.map((shipper) => {
+    var row = document.getElementById("tbody").insertRow(0)
+    row.insertCell(0).innerHTML = shipper.id;
+    row.insertCell(1).innerHTML = shipper.cccd;
+    row.insertCell(2).innerHTML = shipper.ten;
+    row.insertCell(3).innerHTML = shipper.gioitinh;
+    row.insertCell(4).innerHTML = shipper.ngaysinh;
+    row.insertCell(5).innerHTML = shipper.diachi;
+    row.insertCell(6).innerHTML = shipper.sdt;
+    row.insertCell(7).innerHTML = shipper.khuvucgiaohang;
+    row.insertCell(8).innerHTML = shipper.trangthaitiemchung;
+  })
+
     return (
         <div>
             <div className='table-container stat'>
@@ -21,91 +46,7 @@ function ShipperManagement() {
                 </tr>
               </thead>
               <tbody id='tbody'>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
-                <tr>
-                  <td>Item 1</td>
-                  <td>Item 2</td>
-                  <td>Item 3</td>
-                </tr>
+                
               </tbody>
             </table>
           </div>
--- src/pages/KhachHang/Test.jsx
+++ src/pages/KhachHang/Test.jsx
@@ -1,77 +1,48 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import ReactPaginate from "react-paginate";
 
 export default function Test() {
-    const url = 'https://localhost:8001/api/mathang';
-    const [MatHang, setMatHang] = useState([].slice(0, 50));
-    const [pageNumber, setPageNumber] = useState(0);
+    // const url = 'https://localhost:8001/api/mathang';
+    // const [MatHang, setMatHang] = useState([].slice(0, 50));
+    // const [pageNumber, setPageNumber] = useState(0);
 
-    const matHangPerPage = 10;
-    const pagesVisited = pageNumber * matHangPerPage;
-    const displayMatHang = MatHang
-        .slice(pagesVisited, pagesVisited + matHangPerPage)
-        .map(MatHang =>
-            <div className="col-lg-3 col-sm-6 MatHang" key={MatHang.Id}>
-                <div className="sell-item max-width-360">
-                    <div className="product-img">
-                        <img src={MatHang.hinh_anh} />
-                        <div className="product-img-overlay">
-                            <a href="/cart" className="krishok-btn">THÊM VÀO GIỎ HÀNG<i className="fa fa-shopping-cart" /></a>
-                        </div>
-                    </div>
-                    <p><a href="/">{MatHang.ten_mat_hang}</a></p>
-                    <h5>{MatHang.gia_ban.toLocaleString()} vnd | {MatHang.khoi_luong} kg</h5>
-                </div>
-            </div>);
+    // const matHangPerPage = 10;
+    // const pagesVisited = pageNumber * matHangPerPage;
+    // const displayMatHang = MatHang
+    //     .slice(pagesVisited, pagesVisited + matHangPerPage)
+    //     .map(MatHang =>
+    //         <div className="col-lg-3 col-sm-6 MatHang" key={MatHang.Id}>
+    //             <div className="sell-item max-width-360">
+    //                 <div className="product-img">
+    //                     <img src={MatHang.hinh_anh} />
+    //                     <div className="product-img-overlay">
+    //                         <a href="/cart" className="krishok-btn">THÊM VÀO GIỎ HÀNG<i className="fa fa-shopping-cart" /></a>
+    //                     </div>
+    //                 </div>
+    //                 <p><a href="/">{MatHang.ten_mat_hang}</a></p>
+    //                 <h5>{MatHang.gia_ban.toLocaleString()} vnd | {MatHang.khoi_luong} kg</h5>
+    //             </div>
+    //         </div>);
 
-    useEffect(() => {
-        axios.get(url)
-            .then(res => {
-                console.log(res);
-                setMatHang(res.data);
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }, [])
+    // useEffect(() => {
+    //     axios.get(url)
+    //         .then(res => {
+    //             console.log(res);
+    //             setMatHang(res.data);
+    //         })
+    //         .catch(err => {
+    //             console.log(err);
+    //         })
+    // }, [])
 
-    const pageCount = Math.ceil(MatHang.length / matHangPerPage);
-    const changePage = ({ selected }) => {
-        setPageNumber(selected);
-    };
+    // const pageCount = Math.ceil(MatHang.length / matHangPerPage);
+    // const changePage = ({ selected }) => {
+    //     setPageNumber(selected);
+    // };
 
     return (
         <div>
-            <section className="shopping-product ptb-80">
-                <div className="container">
-                    {displayMatHang}
-                    <ReactPaginate
-                        previousLabel={"Previous"}
-                        nextLabel={"Next"}
-                        pageCount={pageCount}
-                        onPageChange={changePage}
-                        containerClassName={"paginationBtn"}
-                        previousLinkClassName={"previousBtn"}
-                        nextLinkClassName={"nextBtn"}
-                        disabledClassName={"paginationDisabled"}
-                        activeClassName={"paginationActive"}
-                    >
-                        {/* <div class="row">
-                            <div class="col-md-12">
-                                <ul class="pagination">
-                                    <li><a href="#"><i class="fa fa-angle-left"></i></a></li>
-                                    <li><a href="#">1</a></li>
-                                    <li><a href="#">2</a></li>
-                                    <li class="active"><a href="#">3</a></li>
-                                    <li><a href="#">4</a></li>
-                                    <li><a href="#"><i class="fa fa-angle-right"></i></a></li>
-                                </ul>
-                            </div>
-                        </div> */}
-                    </ReactPaginate>
-                </div>
-            </section>
+            
         </div>
     );
 };
